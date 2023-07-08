5588dca8d2573a3a04e0eefb4068e66646e7b60d
Dilshoda Abdualimova <dilshoda@Dilshodas-MacBook-Pro.local>
2023-07-08 14:15:26 -0700

    feat: add about component

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+'use client'
+import { useEffect, useState } from 'react';
+
+export default function AboutFeature() {
+
+    return (
+        <main>
+            <h1>About Our App:</h1>
+            <p>This will be a time tracking app</p>
+        </main>
+    )
+}
